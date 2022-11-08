17796939a0d7b9ad440046b581731e1c732f4d3c
belen luna <barbarabluna8@gmail.com>
2022-11-08 15:22:20 -0300

    cambie nombres

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,36 +1,39 @@
 import React from "react";
 import Zapato1 from "../assets/img/corbata.jpg";
+import Zapato2 from "../assets/img/cordones.jpg";
+import Zapato3 from "../assets/img/zapcinto.jpg";
+import Zapato4 from "../assets/img/marrones.jpg";
 
 const ItemDetail = ({ productId }) => {
   if (productId === "1")
     return (
       <div className="product">
         <img src={Zapato1} />
-        <p>Zapato y corbataame</p>
+        <p>Zapato y corbata</p>
       </div>
     );
 
   if (productId === "2")
     return (
       <div className="product">
-        <img src={Zapato1} />
-        <p>Zapato y corbataame</p>
+        <img src={Zapato2} />
+        <p>Acordonados</p>
       </div>
     );
 
   if (productId === "3")
     return (
       <div className="product">
-        <img src={Zapato1} />
-        <p>Zapato y corbataame</p>
+        <img src={Zapato3} />
+        <p>Zapato y cinto</p>
       </div>
     );
 
   if (productId === "4")
     return (
       <div className="product">
-        <img src={Zapato1} />
-        <p>Zapato y corbataame</p>
+        <img src={Zapato4} />
+        <p>Diseños unicos</p>
       </div>
     );
 };
